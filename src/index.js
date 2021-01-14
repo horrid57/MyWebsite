bc4587ae8810387bc705f9bd25860f329e2ab983
alex <alex@clifton.org.uk>
2021-01-14 20:14:41 +0000

    Site v1.1 Refactored my code: - consolidated two classes into one - made the props parameter a single data structure that is defined at the top of the code - removed a lot of unnecessary whitespace and condensed parts - changed the font (good or bad idk i cant tell)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 let greenMode = Math.floor(Math.random() * 10) === 0;
 
-let splashText = [
+let splashTexts = [
     "Website is under construction",
     "¯\\_(ツ)_/¯",
     "Are you mad at me?",
@@ -19,99 +19,60 @@ let splashText = [
     greenMode ? "Damn, green mode is pretty cool" : "Green mode is much more exciting - refresh?",
 ]
 
-let randomInt = Math.floor(Math.random() * splashText.length);
+let splashNumber = Math.floor(Math.random() * splashTexts.length);
 
-let elementText = {
+let elementData = {
     title: {
         text: greenMode ? "c:\\> horrid57.exe" : "horrid57",
-        textStyle: greenMode ? "title-green" : "title",
+        reference: null,
+        className: greenMode ? "title-green" : "title",
         targetDuration: greenMode ? 2000 : 1000,
         initialDelay: 0,
     },
     subtitle: {
-        text: splashText[randomInt],
-        textStyle: greenMode ? "subtitle-green" : "subtitle",
+        text: splashTexts[splashNumber],
+        reference: null,
+        className: greenMode ? "subtitle-green" : "subtitle",
         targetDuration: 2500,
         initialDelay: greenMode ? 2700 : 1700,
     },
     links: [
         {
-            text: "GitHub",
+            text: greenMode ? "> Github" : "GitHub",
             reference: "https://github.com/horrid57",
             className: greenMode ? "link-green" : "github-link",
-            targetDuration: 500,
+            targetDuration: 600,
             initialDelay: greenMode ? 6000 : 5000,
         },
         {
-            text: "Youtube",
+            text: greenMode ? "> Youtube" : "Youtube",
             reference: "https://www.youtube.com/channel/UCWwzXZRV79h2EUbJnKEEe2w",
             className: greenMode ? "link-green" : "youtube-link",
-            targetDuration: 500,
-            initialDelay: greenMode ? 6800 : 5800,
+            targetDuration: 700,
+            initialDelay: greenMode ? 6900 : 5900,
         },
         {
-            text: "Twitch",
+            text: greenMode ? "> Twitch" : "Twitch",
             reference: "https://www.twitch.tv/horrid57",
             className: greenMode ? "link-green" : "twitch-link",
             targetDuration: 500,
-            initialDelay: greenMode ? 7600 : 6600,
+            initialDelay: greenMode ? 7900 : 6900,
         },
         {
-            text: "Steam",
+            text: greenMode ? "> Steam" : "Steam",
             reference: "https://steamcommunity.com/id/horrid57/",
             className: greenMode ? "link-green" : "steam-link",
             targetDuration: 500,
-            initialDelay: greenMode ? 8400 : 7400,
+            initialDelay: greenMode ? 8700 : 7700,
         }
     ]
 }
 
-
-
-class TypedText extends React.Component {
+class TypedElement extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            pointer: -3,
-            text: this.props.text,
-            displayedText: "",
-        }
-    }
-
-    tick() {
-        const _pointer = this.state.pointer;
-        const _targetText = this.state.text;
-        this.setState({
-            displayedText: (this.state.text.substring(0, this.state.pointer) + (_pointer < _targetText.length ? "|" : "")),
-            pointer: _pointer <= _targetText.length + 1 ? _pointer + 1 : _pointer,
-        });
-    }
-
-    async timeout(delay) {
-        return new Promise( res => setTimeout(res, delay) );
-    }
-
-    async componentDidMount() {
-        await this.timeout(this.props.initialDelay)
-        this.interval = setInterval(() => this.tick(), this.props.characterDelay);
-    }
-
-    render() {
-        return (
-            <div className={this.props.textStyle} style={{color: this.state.pointer < 0 ? "black" : null}}>
-                {this.state.displayedText}
-            </div>
-        );
-    }
-}
-
-class InteractiveTypedLink extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: this.props.text,
-            ref: this.props.reference,
-            className: this.props.className,
+            data: props.data,
             displayedText: "",
             pointer: 0,
         }
@@ -119,9 +80,9 @@ class InteractiveTypedLink extends React.Component{
 
     tick() {
         const _pointer = this.state.pointer;
-        const _targetText = this.state.text;
+        const _targetText = this.state.data.text;
         this.setState({
-            displayedText: (this.state.text.substring(0, this.state.pointer) + (_pointer < _targetText.length ? "|" : "")),
+            displayedText: (this.state.data.text.substring(0, this.state.pointer) + (_pointer < _targetText.length ? "|" : "")),
             pointer: _pointer <= _targetText.length + 1 ? _pointer + 1 : _pointer,
         });
     }
@@ -131,92 +92,48 @@ class InteractiveTypedLink extends React.Component{
     }
 
     async componentDidMount() {
-        await this.timeout(this.props.initialDelay)
-        this.interval = setInterval(() => this.tick(), this.props.characterDelay);
+        await this.timeout(this.props.data.initialDelay)
+        this.interval = setInterval(() => this.tick(), (this.state.data.targetDuration / this.state.data.text.length));
     }
 
     render() {
-        return (
-            <div>
-                <a href={this.state.ref}
-                   style={{color: this.state.pointer < 0 ? "black" : null}}
-                   className={this.state.className}
-                   target={"_blank"}
-                   rel={"noreferrer"}
-                >
+        if (this.state.data.reference != null) {
+            return (
+                <a href={this.state.data.reference} style={{color: this.state.pointer < 0 ? "black" : null}}
+                   className={this.state.data.className} target={"_blank"} rel={"noreferrer"} >
                     {this.state.displayedText}
                 </a>
-            </div>
-        );
+            );
+        }
+        else {
+            return (
+                <div className={this.state.data.className} style={{ color: this.state.pointer < 0 ? "black" : null }} >
+                    {this.state.displayedText}
+                </div>
+            );
+        }
+
     }
 }
 
 ReactDOM.render(
-    <div style={{
-        margin: 0,
-        position: "absolute",
-        top: "50%",
-        transform: "translateY(-50%)"
-    }}>
-        <div>
-            <TypedText
-                text={elementText["title"].text}
-                textStyle={elementText["title"].textStyle}
-                characterDelay={elementText["title"].targetDuration / elementText["title"].text.length}
-                initialDelay={elementText["title"].initialDelay}
-            />
-        </div>
-        <TypedText
-            text={elementText["subtitle"].text}
-            textStyle={elementText["subtitle"].textStyle}
-            characterDelay={elementText["subtitle"].targetDuration / elementText["subtitle"].text.length}
-            initialDelay={elementText["subtitle"].initialDelay}
-        />
-
-        <div style={{
-            paddingTop: "30px"
-        }}>
-            <table>
-
-                <tr>
-                    <InteractiveTypedLink
-                        text={elementText["links"][0].text}
-                        reference={elementText["links"][0].reference}
-                        className={elementText["links"][0].className}
-                        characterDelay={elementText["links"][0].targetDuration / elementText["links"][0].text.length}
-                        initialDelay={elementText["links"][0].initialDelay}
-                    />
-                </tr>
-                <tr>
-                    <InteractiveTypedLink
-                        text={elementText["links"][1].text}
-                        reference={elementText["links"][1].reference}
-                        className={elementText["links"][1].className}
-                        characterDelay={elementText["links"][1].targetDuration / elementText["links"][1].text.length}
-                        initialDelay={elementText["links"][1].initialDelay}
-                    />
-                </tr>
-                <tr>
-                    <InteractiveTypedLink
-                        text={elementText["links"][2].text}
-                        reference={elementText["links"][2].reference}
-                        className={elementText["links"][2].className}
-                        characterDelay={elementText["links"][2].targetDuration / elementText["links"][2].text.length}
-                        initialDelay={elementText["links"][2].initialDelay}
-                    />
-                </tr>
-                <tr>
-                    <InteractiveTypedLink
-                        text={elementText["links"][3].text}
-                        reference={elementText["links"][3].reference}
-                        className={elementText["links"][3].className}
-                        characterDelay={elementText["links"][3].targetDuration / elementText["links"][3].text.length}
-                        initialDelay={elementText["links"][3].initialDelay}
-                    />
-                </tr>
-
-            </table>
-        </div>
+    <div style={{ margin: 0, position: "absolute", top: "50%", transform: "translateY(-50%)" }}>
+        <TypedElement data={elementData.title} />
+        <TypedElement data={elementData.subtitle} />
+        <table style={{ paddingTop: "30px" }}>
+            <tr>
+                <TypedElement data={elementData["links"][0]} />
+            </tr>
+            <tr>
+                <TypedElement data={elementData["links"][1]} />
+            </tr>
+            <tr>
+                <TypedElement data={elementData["links"][2]} />
+            </tr>
+            <tr>
+                <TypedElement data={elementData["links"][3]} />
+            </tr>
+        </table>
     </div>,
     document.getElementById('root'),
 );
